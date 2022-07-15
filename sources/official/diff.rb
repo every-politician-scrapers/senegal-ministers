467f6b804ac7affcdf6ba886f8755c35341e1184
Tony Bowden <tony@tmtm.com>
2022-07-15 16:31:27 +0300

    move official comparison to sources/

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -4,6 +4,6 @@
 require 'every_politician_scraper/comparison'
 
 require 'pry'
-diff = EveryPoliticianScraper::DecoratedComparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::DecoratedComparison.new('wikidata.csv', 'scraped.csv').diff
                                          .reject { |row| row.last.to_s.include? "Secrétaire d'Etat" }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
