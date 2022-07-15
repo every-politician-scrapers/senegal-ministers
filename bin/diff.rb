ba5c23a8b4b9e2e0e7799c6a1794147d1f38562b
Tony Bowden <tony@tmtm.com>
2022-07-15 14:26:55 +0300

    Switch to decorated diffs

--- bin/diff.rb
+++ bin/diff.rb
@@ -4,6 +4,6 @@
 require 'every_politician_scraper/comparison'
 
 require 'pry'
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::DecoratedComparison.new('data/wikidata.csv', 'data/official.csv').diff
                                          .reject { |row| row.last.to_s.include? "Secrétaire d'Etat" }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
